0fc6990ba3d57749f054ece4e10d3c36a3474162
Stephen Gowen <sjgowen@gmail.com>
2021-07-04 17:51:12 +0100

    Add project files.

---
+++ Jokes/Data/Migrations/20210704160523_AddedRequiredToFields.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Jokes.Data.Migrations
+{
+    public partial class AddedRequiredToFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeQuestion",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeCreator",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeAnswer",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeQuestion",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeCreator",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeAnswer",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ Jokes/Models/Joke.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jokes.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        [Required] // Makes field non nullable
+        [DisplayName("Joke Question")] // Displays " "'s in the Web Labels
+        public string JokeQuestion { get; set; }
+
+        [Required]
+        [DisplayName("Joke Answer")]
+        public string JokeAnswer { get; set; }
+
+        [Required]
+        [DisplayName("Joke Creator")]
+        public string JokeCreator { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
